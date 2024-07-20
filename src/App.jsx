4e60d77d540851ca3c96c03a648a19d6a6dc6366
Alex Peasey <2>
2024-07-20 21:14:18 +0200

    Working on login for frontend

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,29 @@ import { useState } from "react";
 import Authors from "./components/Authors";
 import Books from "./components/Books";
 import NewBook from "./components/NewBook";
+import LoginForm from "./components/LoginForm"
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import "./styles.css";
 
 const App = () => {
+  const [token, setToken] = useState(null)
+
+  if (!token) {
+    return (
+      <div>
+        <h2>Login</h2>
+        <LoginForm />
+      </div>
+    )
+  }
 
   return (
     <Router>
-      <div>
-        <Link to="/authors">authors</Link><span> | </span>
-        <Link to="/books">books</Link><span> | </span>
+      <div className="nav">
+        <Link to="/authors">authors</Link>
+        <span> | </span>
+        <Link to="/books">books</Link>
+        <span> | </span>
         <Link to="/add">add book</Link>
       </div>
       <Routes>
@@ -19,7 +33,6 @@ const App = () => {
         <Route path="/books" element={<Books />} />
 
         <Route path="/add" element={<NewBook />} />
-
       </Routes>
     </Router>
   );
---
+++ src/components/LoginForm.jsx
@@ -0,0 +1,42 @@
+import { LOGIN } from "./queries";
+import { useMutation } from "@apollo/client";
+import { useEffect, useState } from "react";
+
+const LoginForm = ({ setError, setToken }) => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const [login, result] = useMutation(LOGIN, {
+    onError: (error) => {
+      setError(error.graphQLErrors[0].message);
+    },
+  });
+  useEffect(() => {
+    if (result.data) {
+      const token = result.data.login.value;
+      setToken(token);
+      localStorage.setItem("library-user-token", token);
+    }
+  });
+  const handleLogin = async (event) => {
+    event.preventDefault();
+
+    login({ variables: { username, password } });
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleLogin}>
+        <div>
+          Username <input type="text" value={username} onChange={({ target }) => setUsername(target.value)} />
+        </div>
+        <div>
+          Password <input type="password" value={password} onChange={({ target }) => setPassword(target.value)} />
+        </div>
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+};
+
+export default LoginForm;
