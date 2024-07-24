982a3d870eea4c4b164f74d2e5411ad73ddc5313
Alexander Peasey <alexander.peasey@gmail.com>
2024-07-24 14:27:38 +0200

    completed 8.21 books by genre with GraphQL

--- src/components/Books.jsx
+++ src/components/Books.jsx
@@ -5,21 +5,16 @@ import { useState } from "react";
 const Books = ({ userFaveGenre }) => {
   const [genreFilter, setGenreFilter] = useState("");
 
-  const result = useQuery(GET_BOOKS);
+  const { data, error } = useQuery(GET_BOOKS, {
+    variables: { genre: genreFilter },
+  });
+  if (error) console.log(error.message);
+  if (!data) return;
 
-
-  if (!result.data) return;
-
-  const getAllGenres = () => {
-    const genreArrays = result.data.allBooks.map((entry) => entry.genres);
-    const flattenedGenres = genreArrays.flat();
-    const uniqueGenres = [...new Set(flattenedGenres)];
-    return uniqueGenres;
-  };
 
   return (
     <div>
-      {!userFaveGenre ? <h2>Books</h2> : null }
+      {!userFaveGenre ? <h2>Books</h2> : null}
       <table>
         <tbody>
           <tr>
@@ -27,39 +22,32 @@ const Books = ({ userFaveGenre }) => {
             <th>Author</th>
             <th>Published</th>
           </tr>
-          {result.data.allBooks
-            .filter((book) => {
-              if (genreFilter) {
-                return book.genres.includes(genreFilter);
-              } else if (userFaveGenre) {
-                return book.genres.includes(userFaveGenre)
-              } else {
-                return book;
-              }
-            })
-            .map((a) => (
-              <tr key={a.title}>
-                <td>{a.title}</td>
-                <td>{a.author.name}</td>
-                <td>{a.published}</td>
-              </tr>
-            ))}
+          {data.allBooks.map((a) => (
+            <tr key={a.title}>
+              <td>{a.title}</td>
+              <td>{a.author.name}</td>
+              <td>{a.published}</td>
+            </tr>
+          ))}
         </tbody>
       </table>
-      
-      {!userFaveGenre ? <>
-      <h3>Filter by genre</h3>
-      <div>
-        {getAllGenres().map((genre) => {
-          return (
-            <button key={genre} onClick={() => setGenreFilter(genre)}>
-              {genre}
-            </button>
-          );
-        })}
-        <button onClick={() => setGenreFilter("")}>Reset filters</button>
-      </div> </> : null }
-    </div> 
+
+      {!userFaveGenre ? (
+        <>
+          <h3>Filter by genre</h3>
+          <div>
+            {data.allGenres.map((genre) => {
+              return (
+                <button key={genre} onClick={() => setGenreFilter(genre)}>
+                  {genre}
+                </button>
+              );
+            })}
+            <button onClick={() => setGenreFilter("")}>Reset filters</button>
+          </div>{" "}
+        </>
+      ) : null}
+    </div>
   );
 };
 
